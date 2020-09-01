ba284f612e06c596e101401e2147dc612e5a47e1
David Sung <dwinsung@gmail.com>
2020-09-01 17:07:33 -0500

    second pass, included MAL api validation for user login

--- forms.py
+++ forms.py
@@ -5,5 +5,4 @@ from wtforms.validators import DataRequired, Length
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max = 30)])
-    password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
--- test.py
+++ test.py
@@ -1,17 +1,35 @@
-from flask import Flask, request, render_template, url_for
+from flask import Flask, request, render_template, url_for, flash, redirect
 from forms import LoginForm
+import requests
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = '92fa7fd80fcb9e428cb33c697e4e9b5f'
+app.config['SECRET_KEY'] 
+
+def validate_MAL_login(user):
+    try:
+        r = requests.get('https://api.jikan.moe/v3/user/' + user)
+        if r.status_code == 200:
+            return True
+        else:
+            return False
+    except requests.exceptions.HTTPError as e:
+        return False
 
 @app.route("/", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    user = form.username.data
+    if form.validate_on_submit():
+        if  validate_MAL_login(user):
+            flash(f'Account login successful for {user}!', 'success')
+            return redirect(url_for('login'))
+        else:
+            flash(f'This is not a valid MyAnimeList account.')
     return render_template('login.html', title = 'Login', form=form)
     
-@app.route("/about")
-def about():
-    return "<h1>About Page </h1>"
+@app.route("/profile")
+def profile():
+    return "<h1>Profile Page </h1>"
 
 if __name__ == '__main__':
     app.run(debug=True)
