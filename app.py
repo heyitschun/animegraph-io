6fbc2e521637f6bf8be0ea090ca7da6858fd806b
David Sung <dwinsung@gmail.com>
2020-09-08 22:13:37 -0500

    API call now implements database optimization

--- app.py
+++ app.py
@@ -3,12 +3,34 @@ from forms import LoginForm
 import requests
 import json
 from operator import itemgetter 
-app = Flask(__name__)
 import os
 from bs4 import BeautifulSoup
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import select
 
+app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(32)
+POSTGRES = {
+    'user': 'username',
+    'pw': 'password',
+    'db': 'anime_list',
+    'host': 'localhost',
+    'port': '5432',
+}
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
+%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+
+db = SQLAlchemy(app)
+
+class animes(db.Model):
+    __tablename__ = 'animes'
+    anime_id = db.Column('anime_id', db.INTEGER, primary_key=True)
+    title = db.Column('title', db.VARCHAR)
+    genre = db.Column('genre', db.VARCHAR)
+
+    
+from app import db
 @app.route("/api/get-top-ten")
 def get_user_watch_history():
     #gets list of top 10 rated animes by user, anime info stored in dictionaries.
@@ -19,11 +41,28 @@ def get_user_watch_history():
         anime_history = sorted(userdata['anime'], key=itemgetter('score'))
         for anime in anime_history:
             anime_id = anime['mal_id']
-            anime_genres = helper_get_genre_of_anime(anime_id)
+            anime_genres = get_genre_from_database(anime_id)
             anime['genres']= anime_genres['genres']
         return {"data": anime_history, "statusCode": 200}
     else:
         return {"data": "Failed", "statusCode": 400}
+
+def anime_in_database(test_id):
+    genre = animes.query.filter_by(anime_id= test_id).first()
+    return genre
+
+def get_genre_from_database(test_id):
+    anime_exists = anime_in_database(test_id)
+    genre_list = []
+    if anime_exists is None:
+        # if not in database, grabs from webscrape
+        test_id_genres = helper_get_genre_of_anime(test_id)
+    else:
+        # database needs to be in list format for this to work.
+        genre_list.append(anime_exists.genre)
+        test_id_genres = {"genres": genre_list}
+    return test_id_genres
+
 
 @app.route("/api/get-anime-genre")
 def get_anime_genre():
@@ -32,7 +71,6 @@ def get_anime_genre():
         return {"data": "Failed", "statusCode": 400}
 
     return helper_get_genre_of_anime(request.args.get("id"))
-
 def helper_get_genre_of_anime(anime_id):
     #gets genre of anime given ID, scraped from MAL using BeautifulSoup, can replace later w database.
     try:
@@ -45,7 +83,6 @@ def helper_get_genre_of_anime(anime_id):
             genre_list.append(g.string)
         return {"genres": genre_list, "statusCode": 200}
     except Exception as e:
-        print(e)
         return {"errorMessage": "Errored out", "statusCode": 400}
 
 
